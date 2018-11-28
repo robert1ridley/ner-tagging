cb165f997e3ba5250bde10d8244221aa42ae9ff0
robert1ridley <robert.n.ridley@gmail.com>
2018-11-28 18:04:27 +0800

    get tag sequences

--- models/HMM.py
+++ models/HMM.py
@@ -14,12 +14,25 @@ def calculate_emissions(sentences):
                 emission_count[pair[0]] = 1
             else:
                 emission_count[pair[0]] += 1
+    return emission_count
+
+
+def get_tag_seqs(sentences):
+    sets = []
+    for sentence in sentences:
+        tags = []
+        for pair in sentence:
+            tags.append(pair[1])
+        sets.append(tags)
+    return sets
 
 
 def main():
     data = load_file('../data/train.txt')
     sentences = get_pairs(data)
     calculate_emissions(sentences)
+    tag_seqs = get_tag_seqs(sentences)
+    print(tag_seqs[:20])
 
 
 if __name__ == '__main__':
