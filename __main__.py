a2ad0e0334f1124b5f473dd897e1d28e88552993
Robert Ridley <robert.n.ridley@gmail.com>
2018-11-28 12:46:00 +0800

    main update

--- __main__.py
+++ __main__.py
@@ -6,21 +6,36 @@ RARE_WORD_MAX_FREQ = 5
 LOG_PROB_OF_ZERO = -1000
 
 def split_words_and_tags(training_data):
-  training_tags = [[wordtag.rsplit('/', 1)[-1] for wordtag in sentence.strip().split()] for sentence in training_data]
-  training_tags = [[START_SYMBOL] * 2 + sent_tags + [STOP_SYMBOL] for sent_tags in training_tags]
-
-  training_words = [[wordtag.rsplit('/', 1)[0] for wordtag in sentence.strip().split()] for sentence in training_data]
-  training_words = [[START_SYMBOL] * 2 + sent_words + [STOP_SYMBOL] for sent_words in training_tags]
+  training_tags = []
+  training_words = []
+  for sentence in training_data:
+    current_sentence_words = []
+    current_sentence_tags = []
+    sentence = (START_SYMBOL + "/" + START_SYMBOL + " ") * 2 + sentence + " " + STOP_SYMBOL + "/" + STOP_SYMBOL
+    for word in sentence.strip().split():
+      splits = word.rsplit('/', 1)
+      current_sentence_words.append(splits[0])
+      current_sentence_tags.append(splits[1])
+    training_words.append(current_sentence_words)
+    training_tags.append(current_sentence_tags)
   return training_words, training_tags
+
+
+def get_words_and_tags(data):
+  word_tag_list = []
+  for word in data.strip().split():
+    splits = word.rsplit('/', 1)
+    word_tag_list.append(splits)
+  return word_tag_list
 
 
 def main():
   infile = open('./data/dev.txt', 'r')
-  data = infile.readlines()
+  data = infile.read()
   infile.close()
 
-  words, tags = split_words_and_tags(data)
-  print(words)
+  word_tag_pairs = get_words_and_tags(data)
+
 
 
 if __name__ == '__main__':
