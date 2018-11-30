83fcc197084b23d7469da2bc47b5b47b6feee99a
Robert Ridley <robert.n.ridley@gmail.com>
2018-11-30 17:58:05 +0800

    argmax

--- models/HMM.py
+++ models/HMM.py
@@ -15,7 +15,7 @@ def viterbi(states, sentence, transition, emission):
   sentence.insert(0, '*')
   sentence.insert(0, '*')
   sentence.append('STOP')
-  paths = [[]]
+  paths = []
 
   # Calc initial paths
   index = 2
@@ -29,20 +29,19 @@ def viterbi(states, sentence, transition, emission):
       if current_tag in emission.keys():
         if initial_word in emission[current_tag].keys():
           probability = float(emission[current_tag][initial_word]) * float(transition[item])
-          paths[0].append((item, probability))
+          paths.append([(item, probability)])
   index += 1
 
-  # Calc remaining paths
   while index < len(sentence):
     current_word = sentence[index]
-    prev_instances = paths[-1]
-    paths.append([])
-    for item in prev_instances:
-      max_prob = -100
+    print("RUN")
+    point_count = 0
+    for point in paths:
+      tag1 = point[-1][0][1]
+      tag2 = point[-1][0][2]
+      prob = point[-1][1]
       best_set = ()
-      tag1 = item[0][1]
-      tag2 = item[0][2]
-      prob = item[1]
+      max_prob = -100
       word_in_dict = is_in_emissions_dict(current_word, emission)
       if not word_in_dict:
         current_word = '_RARE_'
@@ -52,29 +51,30 @@ def viterbi(states, sentence, transition, emission):
           if current_tag in emission.keys():
             if current_word in emission[current_tag].keys():
               probability = float(emission[current_tag][current_word]) * float(transition[tran])
-              if (tran, probability) not in paths[-1]:
-                if probability*prob > max_prob*prob:
-                  max_prob = probability
-                  best_set = tran
+              if probability*prob > max_prob*prob:
+                max_prob = probability
+                best_set = tran
       if best_set != ():
-        paths[-1].append((best_set, max_prob))
+        paths[point_count].append((best_set, max_prob))
+      point_count += 1
     index += 1
-  rev = paths[::-1]
-  print(rev)
 
-  # Backpointers calc
-  # path_options = {}
-  # ind = 0
-  # for set_of_choices in rev:
-  #   for choice in set_of_choices:
-  #     if ind == 0:
-  #       path_options[choice[0]] = {}
-  #     if ind != 0:
-  #       current_tag = choice[0][2]
-  #       prev_tag = choice[0][1]
-  #       for
-  #   ind += 1
-  # print(path_options)
+  # ARGMAX
+  best_prob_val = -100
+  best_set = []
+  for path in paths:
+    prob_val = 0
+    if len(path) == len(sentence)-2:
+      for node in path:
+        if prob_val == 0:
+          prob_val = node[1]
+        else:
+          prob_val *= node[1]
+    if prob_val > best_prob_val:
+      best_prob_val = prob_val
+      best_set = path
+  print(best_prob_val)
+  print(best_set)
 
 
 
