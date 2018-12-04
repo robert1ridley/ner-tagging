4fff5a262619b1d131bda763233e6227f50bcfec
Robert Ridley <robert.n.ridley@gmail.com>
2018-12-04 09:39:07 +0800

    renaming

--- models/Viterbi.py
+++ models/Viterbi.py
@@ -21,37 +21,37 @@ class Viterbi(object):
       return 0.0
     if (idx, item) in self.observation_tag_probability:
       return self.observation_tag_probability[idx, item][0]
-    if observations[idx - 1] not in self.word_tags_set:
-      first_item = self.unique_tags
-    else:
-      first_item = self.word_tags_set[observations[idx - 1]]
     if observations[idx - 2] not in self.word_tags_set:
-      second_item = self.unique_tags
+      initial_observation = self.unique_tags
     else:
-      second_item = self.word_tags_set[observations[idx - 2]]
+      initial_observation = self.word_tags_set[observations[idx - 2]]
+    if observations[idx - 1] not in self.word_tags_set:
+      second_observation = self.unique_tags
+    else:
+      second_observation = self.word_tags_set[observations[idx - 1]]
     max_prob = float('-inf')
     pointer = "*"
-    for first_ob_tag in first_item:
-      for second_ob_tag in second_item:
+    for second_obs_tag in second_observation:
+      for initial_obs_tag in initial_observation:
         probability = 0.0
-        if (second_ob_tag, first_ob_tag, item) in self.transition_probabilities:
-          transition_prob = self.transition_probabilities[(second_ob_tag, first_ob_tag, item)]
+        if (initial_obs_tag, second_obs_tag, item) in self.transition_probabilities:
+          transition_prob = self.transition_probabilities[(initial_obs_tag, second_obs_tag, item)]
         else:
-          if (second_ob_tag, first_ob_tag) in self.bigram_counts:
+          if (initial_obs_tag, second_obs_tag) in self.bigram_counts:
             transition_prob = math.log(
-              1.0 / float(self.bigram_counts[(second_ob_tag, first_ob_tag)] + self.no_tags)
+              1.0 / float(self.bigram_counts[(initial_obs_tag, second_obs_tag)] + self.no_tags)
             )
           else:
             transition_prob = math.log(1.0 / float(self.no_tags))
         if (observations[idx], item) not in self.emission_probabilities:
           transition_prob = 0.0
-          probability = self.calc_backpointers(observations, idx - 1, first_ob_tag) + transition_prob
+          probability = self.calc_backpointers(observations, idx - 1, second_obs_tag) + transition_prob
         else:
-          probability = self.calc_backpointers(observations, idx - 1, first_ob_tag) + \
+          probability = self.calc_backpointers(observations, idx - 1, second_obs_tag) + \
                         self.emission_probabilities[(observations[idx], item)] + transition_prob
         if max_prob < probability:
           max_prob = probability
-          pointer = first_ob_tag
+          pointer = second_obs_tag
     self.observation_tag_probability[idx, item] = (max_prob, (idx - 1, pointer))
     return max_prob
 
